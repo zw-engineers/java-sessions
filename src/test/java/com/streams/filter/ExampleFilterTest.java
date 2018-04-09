79d43c2462d77ec8ee42c0f7abed11525feb0095
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-04-09 12:34:31 +0100

    add another Filter unit test

--- src/test/java/com/streams/filter/ExampleFilterTest.java
+++ src/test/java/com/streams/filter/ExampleFilterTest.java
@@ -1,7 +1,11 @@
 package com.streams.filter;
 
+import com.streams.Person;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,7 +19,16 @@ public class ExampleFilterTest {
     }
 
     @Test
-    public void return_list_of_people_over_20_years_old() {
+    public void return_number_of_people_over_20_years_old() {
         assertEquals(2, exampleFilter.peopleOver20().size());
+    }
+
+    @Test
+    public void return_list_of_people_over_20_years_old() {
+        List<Person> people = Arrays.asList(
+                new Person("Artemas", 28),
+                new Person("Tom", 40)
+        );
+        assertEquals(people, exampleFilter.peopleOver20());
     }
 }
