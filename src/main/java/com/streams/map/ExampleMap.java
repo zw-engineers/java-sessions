546634bea3248b14ac145a8a496fc15f28f87138
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-04-12 12:27:00 +0100

    add more Map Operation content

--- src/main/java/com/streams/map/ExampleMap.java
+++ src/main/java/com/streams/map/ExampleMap.java
@@ -9,7 +9,7 @@ public class ExampleMap {
         ToIntFunction<String> mapper = Integer::parseInt;
 
         return list.stream() // Here we declare our Stream.
-                .mapToInt(mapper) // Here we
-                .sum();
+                .mapToInt(mapper) // Here we map our Strings values so they can be converted into integer values.
+                .sum(); // After they are Converted into integer values we can then perform our sum operation.
     }
 }
