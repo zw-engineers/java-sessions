f47eaf5b8cf9074e40c63da275d22750a63620fa
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-11-20 19:52:42 +0000

    Add case when one of the list values is null

--- src/main/java/com/functional/FunctionalInDepth.java
+++ src/main/java/com/functional/FunctionalInDepth.java
@@ -1,6 +1,10 @@
 package com.functional;
 
+import io.vavr.Value;
 import io.vavr.collection.List;
+import io.vavr.control.Option;
+
+import java.util.Objects;
 
 
 class FunctionalInDepth {
@@ -8,7 +12,11 @@ class FunctionalInDepth {
     List<String> getNamesCapitalised(List<String> names) {
         // In Java 8 streams...
 //        names.stream().map(String::toUpperCase).collect(Collectors.toList());
-        return names
+        return Option.of(names)
+                .getOrElse(List::empty)
+                .map(Option::of)
+                .map(Value::getOrNull)
+                .filter(Objects::nonNull)
                 .map(String::toUpperCase)
                 .collect(List.collector());
     }
