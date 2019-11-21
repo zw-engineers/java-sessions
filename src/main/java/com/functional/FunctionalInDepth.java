a11956a6948cdea3d63cc5e51a744b6f63475b58
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-11-21 00:07:48 +0000

    refactor FunctionalInDepth

--- src/main/java/com/functional/FunctionalInDepth.java
+++ src/main/java/com/functional/FunctionalInDepth.java
@@ -1,10 +1,7 @@
 package com.functional;
 
-import io.vavr.Value;
 import io.vavr.collection.List;
 import io.vavr.control.Option;
-
-import java.util.Objects;
 
 
 class FunctionalInDepth {
@@ -12,11 +9,19 @@ class FunctionalInDepth {
     List<String> getNamesCapitalised(List<String> names) {
         // In Java 8 streams...
 //        names.stream().map(String::toUpperCase).collect(Collectors.toList());
+
+//        Option.of(names)
+//                .getOrElse(List::empty)
+//                .map(Option::of)
+//                .map(Value::getOrNull)
+//                .filter(Objects::nonNull)
+//                .map(String::toUpperCase)
+//                .collect(List.collector());
+
         return Option.of(names)
                 .getOrElse(List::empty)
                 .map(Option::of)
-                .map(Value::getOrNull)
-                .filter(Objects::nonNull)
+                .flatMap(name -> name)
                 .map(String::toUpperCase)
                 .collect(List.collector());
     }
