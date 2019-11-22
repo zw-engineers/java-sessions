5c5d156ba3e7eaa9d05d23424b3975949157df21
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-11-22 13:21:39 +0000

    convert to vavr List

--- src/main/java/com/functional/University.java
+++ src/main/java/com/functional/University.java
@@ -1,14 +1,11 @@
 package com.functional;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import io.vavr.collection.List;
 
 class University {
     List<Degree> assignToCompScienceDegree(List<Student> students) {
         return students
-                .stream()
                 .map(student -> new Degree("computer science", student, new Tutor("thomas", "jenkins")))
-                .peek(System.out::println)
-                .collect(Collectors.toList());
+                .collect(List.collector());
     }
 }
--- src/test/java/com/functional/UniversityTest.java
+++ src/test/java/com/functional/UniversityTest.java
@@ -1,10 +1,10 @@
 package com.functional;
 
+import io.vavr.collection.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
