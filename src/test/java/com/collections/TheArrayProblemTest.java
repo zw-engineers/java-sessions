676c8c3b495918def3adc8383fa8fdf3d6d0905c
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-10-25 11:45:48 +0000

    convert unit test to Junit 5

--- src/test/java/com/collections/TheArrayProblemTest.java
+++ src/test/java/com/collections/TheArrayProblemTest.java
@@ -1,27 +1,20 @@
 package com.collections;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TheArrayProblemTest {
-
-    private TheArrayProblem theArrayProblem;
-
-    @Before
-    public void init() {
-        theArrayProblem = new TheArrayProblem();
-    }
-
     @Test
+    @DisplayName("Should return a new array with a new element")
     public void should_return_a_new_array_with_a_new_element() {
         Product door = new Product("Door", 35);
         Product floorPanel = new Product("Floor Panel", 25);
         Product window = new Product("Window", 10);
 
         Product[] products = {door, floorPanel};
-        products = theArrayProblem.add(window, products);
+        products = TheArrayProblem.add(window, products);
         assertThat(products.length).isEqualTo(3);
     }
 
