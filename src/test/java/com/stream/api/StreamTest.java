48a02dcb620156b252bce6336977c22abe9552d7
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-10-25 17:23:15 +0000

    convert test to junit 5 test

--- src/test/java/com/stream/api/StreamTest.java
+++ src/test/java/com/stream/api/StreamTest.java
@@ -9,13 +9,14 @@ import static com.stream.api.StreamExample.convertToStream;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class StreamTest {
+
     private final static Integer someNumber = null;
 
     @Test
     @DisplayName("Should return an empty Stream when the value to be streamed is null")
     void nullStream() {
         Stream<Integer> valueStream = convertToStream(someNumber);
-
-        assertThat(valueStream).isEmpty(); // Bare in mind that Empty != null...
+        // Bare in mind that Empty != null...
+        assertThat(valueStream).isEmpty();
     }
 }
--- src/test/java/com/stream/api/SumArraysArraysTest.java
+++ src/test/java/com/stream/api/SumArraysArraysTest.java
@@ -1,14 +1,18 @@
 package com.stream.api;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-public class SumArraysArraysTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SumArraysArraysTest {
+
     @Test
-    public void sampleTests() {
-        assertEquals(21, SumArrays.arrayPlusArray(new int[]{1,2,3}, new int[]{4,5,6}));
-        assertEquals(-21, SumArrays.arrayPlusArray(new int[]{-1,-2,-3}, new int[]{-4,-5,-6}));
-        assertEquals(15, SumArrays.arrayPlusArray(new int[]{0,0,0}, new int[]{4,5,6}));
-        assertEquals(2100, SumArrays.arrayPlusArray(new int[]{100,200,300}, new int[]{400,500,600}));
+    @DisplayName("Sample Tests")
+    void sampleTests() {
+        assertEquals(21, SumArrays.arrayPlusArray(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }));
+        assertEquals(-21, SumArrays.arrayPlusArray(new int[] { -1, -2, -3 }, new int[] { -4, -5, -6 }));
+        assertEquals(15, SumArrays.arrayPlusArray(new int[] { 0, 0, 0 }, new int[] { 4, 5, 6 }));
+        assertEquals(2100, SumArrays.arrayPlusArray(new int[] { 100, 200, 300 }, new int[] { 400, 500, 600 }));
     }
 }
