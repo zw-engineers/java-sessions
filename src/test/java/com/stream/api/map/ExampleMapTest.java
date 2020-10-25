0282beb8fa053bf05491ef26a28bd6020a05002b
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-10-25 17:14:03 +0000

    convert test to junit 5 test

--- src/test/java/com/stream/api/map/ExampleMapTest.java
+++ src/test/java/com/stream/api/map/ExampleMapTest.java
@@ -1,24 +1,26 @@
 package com.stream.api.map;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ExampleMapTest {
+class ExampleMapTest {
 
     private ExampleMap exampleMap;
 
-    @Before
-    public void before() {
+    @BeforeEach
+    void before() {
         exampleMap = new ExampleMap();
     }
 
     @Test
-    public void should_return_count_of_lists_in_list(){
+    @DisplayName("Should return count of lists in a list")
+    void should_return_count_of_lists_in_list(){
 
         List<String> list = Arrays.asList("1", "2", "3", "4", "5");
 
